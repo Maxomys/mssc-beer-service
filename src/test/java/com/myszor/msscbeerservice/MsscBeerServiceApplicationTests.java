216ce7424172744333a5688a7f151ba1a612a7d6
Zaphod <mistrz7losos@live.com>
2020-11-04 05:06:23 +0100

    Initial commit

---
+++ src/test/java/com/myszor/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myszor.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
