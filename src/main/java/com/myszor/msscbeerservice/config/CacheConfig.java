d5e851b3c936c05a51ab80152b34405ef16b760f
Zaphod <mistrz7losos@live.com>
2021-01-06 01:36:35 +0100

    Added ehcache

---
+++ src/main/java/com/myszor/msscbeerservice/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package com.myszor.msscbeerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+
+
+}
--- src/main/java/com/myszor/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/com/myszor/msscbeerservice/services/BeerServiceImpl.java
@@ -7,6 +7,7 @@ import com.myszor.msscbeerservice.web.mappers.BeerMapper;
 import com.myszor.msscbeerservice.web.model.BeerDto;
 import com.myszor.msscbeerservice.web.model.BeerPagedList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false")
     @Override
     public BeerPagedList listBeers(String beerName, String beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
         BeerPagedList beerPagedList;
@@ -64,6 +66,7 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
         if (showInventoryOnHand) {
